45d857aebbc0c71ae328a5848b16d5130d92214b
luoyu <18715838915@163.com>
2024-06-26 06:59:31 +0800

    增加日志配置

--- services/hospital/src/main/java/com/luobd/server/controller/HospitalController.java
+++ services/hospital/src/main/java/com/luobd/server/controller/HospitalController.java
@@ -5,6 +5,7 @@ import com.luobd.server.business.hospital.entity.HospitalSet;
 import com.luobd.server.business.hospital.service.IHospitalSetService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import javax.annotation.Resource;
 @RestController
 @Api(tags = "医院模块")
 @RequestMapping(value = "/api/hospital")
+@Slf4j
 public class HospitalController {
 
 
@@ -26,6 +28,7 @@ public class HospitalController {
     @GetMapping(value = "/test")
     @ApiOperation(value = "测试接口")
     public String test(@RequestParam(value = "id") String id) {
+        log.info("test");
         HospitalSet set = hospitalSetService.getById(1L);
         System.out.println(1/0);
         return  id + "success";
